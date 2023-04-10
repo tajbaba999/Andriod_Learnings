3c29c15492321b2b72074ba7b309c568be5f22a1
tajbaba999 <tajbaba153@gmail.com>
2023-04-10 23:40:05 +0530

    Web view

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.webview
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webviewvariable = findViewById<WebView>(R.id.webView)
+        webViewSetUp(webviewvariable)
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetUp(webView: WebView){
+
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://polygon.technology/")
+        }
+    }
+}
